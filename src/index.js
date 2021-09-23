5c9e5e611f52bb47d40fcef640d69232e3afae13
Hiroe Araki <hiroe.ar.1017@gmail.com>
2021-09-23 19:46:30 +0900

    Updated function showCity

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-function showCity() {
-  let city = document.querySelector("#search-city").value;
+function showCity(city) {
   let apiKey = "8d78900de96863eec7f22dd9ac02260d";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(ShowSearchTemp);
 }
 
 function handleSubmit(event) {
-event.preventDefault();
- let city = document.querySelector("#search-city").value;
-showCity(city);
+  event.preventDefault();
+  let city = document.querySelector("#search-city").value;
+  showCity(city);
 }
 
 let search = document.querySelector("form");
 search.addEventListener("submit", handleSubmit);
 
 function ShowSearchTemp(response) {
- document.querySelector("#input-city").innerHTML = response.data.name;
- document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
- document.querySelector("#description").innerHTML = response.data.weather[0].main;
- document.querySelector("#temp-max").innerHTML = Math.round(response.data.main.temp_max);
- document.querySelector("#temp-min").innerHTML = Math.round(response.data.main.temp_min);
+  document.querySelector("#input-city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#temp-max").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#temp-min").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
 }
- 
- let now = new Date();
- let date = now.getDate();
- let dateNow = document.querySelector("#date-now");
+
+let now = new Date();
+let date = now.getDate();
+let dateNow = document.querySelector("#date-now");
 dateNow.innerHTML = `${date}`;
 
 let months = [
@@ -39,19 +45,18 @@ let months = [
   "Sep",
   "Oct",
   "Nov",
-  "Dec"
+  "Dec",
 ];
 let month = months[now.getMonth()];
 let monthNow = document.querySelector("#months-now");
 monthNow.innerHTML = `${month}`;
 
-
- let hour = now.getHours();
- let hourNow = document.querySelector("#hours-now");
- hourNow.innerHTML = `${hour}`;
- if (hour > 10) {
-   hour = `0${hour}`;
- }
+let hour = now.getHours();
+let hourNow = document.querySelector("#hours-now");
+hourNow.innerHTML = `${hour}`;
+if (hour > 10) {
+  hour = `0${hour}`;
+}
 
 let minute = now.getMinutes();
 let minuteNow = document.querySelector("#minutes-now");
@@ -64,15 +69,7 @@ let year = now.getFullYear();
 let yearNow = document.querySelector("#year-now");
 yearNow.innerHTML = `${year}`;
 
-let days = [
-  "Sun",
-  "Mon",
-  "Tue",
-  "Wed",
-  "Thu",
-  "Fri",
-  "Sat"
-];
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let day = days[now.getDay()];
 let dayNow = document.querySelector("#day-now");
 dayNow.innerHTML = `${day}`;
@@ -85,11 +82,11 @@ function positionNow(position) {
   axios.get(url).then(ShowSearchTemp);
 }
 
-function getCurrentPosition(){
-navigator.geolocation.getCurrentPosition(positionNow);
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(positionNow);
 }
 
-let button = document.querySelector("#search-current")
+let button = document.querySelector("#search-current");
 button.addEventListener(`click`, getCurrentPosition);
 
 showCity("Tokyo");
